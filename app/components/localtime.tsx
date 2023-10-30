e27c01991c835568329e0d01d589ce8e376149d2
Roland Kajatin <roland.kajatin@gmail.com>
2023-10-30 19:43:10 +0100

    more info in ics file

--- app/components/localtime.tsx
+++ app/components/localtime.tsx
@@ -1,34 +1,46 @@
 "use client";
 
 import moment from "moment";
-import { createEvent, EventAttributes } from 'ics';
+import { createEvent, EventAttributes } from "ics";
 
 async function handleDownload(event: any) {
-  const moment_start = moment(event.start)
-  const duration = event.duration
-  const moment_end = moment_start.add({ "minutes": duration })
-
+  console.log(event);
   const details = {
-    title: "Padel Club",
-    start: moment_start.format('YYYY-M-D-H-m').split("-").map(Number),
-    end: moment_end.format("YYYY-M-D-H-m").split("-").map(Number)
-  } as EventAttributes
+    title: event.title,
+    description:
+      "Padel session with " +
+      event.participants.map((p: any) => p.name).join(", ") +
+      " (more might sign up later too).",
+    location: event.location,
+    start: [
+      event.start.getFullYear(),
+      event.start.getMonth() + 1,
+      event.start.getDate(),
+      event.start.getHours(),
+      event.start.getMinutes(),
+    ],
+    duration: { minutes: event.duration },
+    url: "https://padel.rolandkajatin.com/sessions/" + event.id,
+    status: event.booked ? "CONFIRMED" : "TENTATIVE",
+    categories: ["padel"],
+    classification: event.private ? "PRIVATE" : "PUBLIC",
+  } as EventAttributes;
 
-  const filename = 'padel.ics'
-  const file = await new Promise((resolve, reject) => {
+  const filename = "padel.ics";
+  const file = (await new Promise((resolve, reject) => {
     createEvent(details, (error, value) => {
       if (error) {
-        reject(error)
+        reject(error);
       }
 
-      resolve(new File([value], filename, { type: 'text/calendar' }))
-    })
-  }) as Blob;
+      resolve(new File([value], filename, { type: "text/calendar" }));
+    });
+  })) as Blob;
   const url = URL.createObjectURL(file);
 
   // trying to assign the file URL to a window could cause cross-site
   // issues so this is a workaround using HTML5
-  const anchor = document.createElement('a');
+  const anchor = document.createElement("a");
   anchor.href = url;
   anchor.download = filename;
 
@@ -41,5 +53,12 @@ async function handleDownload(event: any) {
 
 export default function LocalTime(props: { event: any }) {
   const { event } = props;
-  return <div onClick={() => handleDownload(event)}>{moment(event.start).local().format("LLLL")}</div>;
+  return (
+    <div
+      className="cursor-pointer hover:underline"
+      onClick={() => handleDownload(event)}
+    >
+      {moment(event.start).local().format("LLLL")}
+    </div>
+  );
 }
