2cf33f1f3cfe7d9e2c09eb37d1263b45be8745cf
Roland Kajatin <roland.kajatin@gmail.com>
2023-11-05 12:27:13 +0100

    simpler menu

--- app/hero.tsx
+++ app/hero.tsx
@@ -32,26 +32,39 @@ export default function Hero() {
           <div className="hover:text-slate-300">Sign in</div>
         </Link>
       ) : (
-        <Link href={`/users/${sub}`} className="hover:scale-105 transition-all">
-          {session?.user?.image ? (
-            <Image
-              width={32}
-              height={32}
-              className="h-8 w-8 rounded-full"
-              src={session.user.image}
-              alt={username}
-            />
-          ) : (
-            <div
-              className="flex w-8 h-8 rounded-full font-medium items-center justify-center"
-              style={{
-                backgroundColor: stringToColor(username),
-              }}
-            >
-              {generateMonogram(username)}
-            </div>
-          )}
-        </Link>
+        <div className="flex flex-row gap-3 items-center">
+          <Link href="/merch" className="uppercase hover:underline">
+            Merch
+          </Link>
+
+          <div className="opacity-70">|</div>
+
+          <Link
+            href={`/users/${sub}`}
+            className="flex flex-row gap-2 items-center transition-all"
+          >
+            <div className="uppercase hover:underline">Profile</div>
+
+            {session?.user?.image ? (
+              <Image
+                width={32}
+                height={32}
+                className="h-8 w-8 rounded-full hover:scale-105"
+                src={session.user.image}
+                alt={username}
+              />
+            ) : (
+              <div
+                className="flex w-8 h-8 rounded-full font-medium items-center justify-center hover:scale-105"
+                style={{
+                  backgroundColor: stringToColor(username),
+                }}
+              >
+                {generateMonogram(username)}
+              </div>
+            )}
+          </Link>
+        </div>
       )}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "./hero";
-import Merch from "./merch";
 import Events from "./events";
 import Members from "./members";
 import FAQ from "./faq";
@@ -13,7 +12,6 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-10 max-w-xl w-full">
       <Hero />
-      <Merch />
       <Events />
       <Members />
       <FAQ />
